af51b7a767cc9559f7a7d812a811a79a4875cfba
oksana <miss.okcana-chernova@yandex.ru>
2023-02-09 23:33:29 +0300

    h3

--- homework/oop/src/main/java/less1/Woman.java
+++ homework/oop/src/main/java/less1/Woman.java
@@ -1,6 +1,8 @@
 package less1;
 
 public class Woman extends Human{
+
+    Human husband;
     public Woman(String name, String birthdate, String death, Human mother, Human father, Human child) {
         super(name, birthdate, death, mother, father, child);
     }
